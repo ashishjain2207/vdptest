0dd7e141084084c69e11c8cdcefa5515be842e49
Ashish Jain <ashish.jain@imriva.de>
2026-07-10 16:24:41 +0530

    Remove Playwright E2E suite and workflow

--- e2e/tests/accessControl.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test } from '../fixtures/test';
-
-test.describe('access control', () => {
-  test('guest user cannot access home feed', async ({ guestUser, homeFeedPage }) => {
-    await guestUser.reset();
-    await homeFeedPage.goto();
-    await homeFeedPage.expectAccessDeniedRedirect();
-  });
-});
